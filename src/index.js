a60a40bf4dda62d711115f187790ee217499254b
Joris Tirado <joris@tirado.com>
2017-11-07 12:39:06 -0800

    Use App component as common owner to hold the state

--- src/index.js
+++ src/index.js
@@ -2,15 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
-const devRows = [{
-  name: 'paul',
-  total: 5
-},{
-  name: 'henry',
-  total: 3
-},{
-  name: 'mathieu',
-  total: 1
-}];
-
-ReactDOM.render(<App rows={devRows} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
