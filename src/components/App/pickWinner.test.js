aaf7715ed75e074e8ae230645a5624cd2350cca2
Joris Tirado <joris@tirado.com>
2017-11-16 11:22:07 -0800

    Restructure test suite with `describe()`

--- src/components/App/pickWinner.test.js
+++ src/components/App/pickWinner.test.js
@@ -1,28 +1,30 @@
 import pickWinner, { createBucket, randomIndex } from './pickWinner';
 
-const rows = [
-  { name: 'paul', total: 3 },
-  { name: 'henri', total: 2 },
-  { name: 'jacques', total: 1 }
-];
-const expectedBucket = ['paul', 'paul', 'paul', 'henri', 'henri', 'jacques'];
-const expectedIndexes = [0, 1, 2, 3, 4, 5];
+describe('pickWinner', () => {
+  const rows = [
+    { name: 'paul', total: 3 },
+    { name: 'henri', total: 2 },
+    { name: 'jacques', total: 1 }
+  ];
+  const expectedBucket = ['paul', 'paul', 'paul', 'henri', 'henri', 'jacques'];
+  const expectedIndexes = [0, 1, 2, 3, 4, 5];
 
-test('unpack rows correctly', () => {
-  const bucket = createBucket(rows);
-  expect(bucket).toEqual(expectedBucket);
-});
+  it('unpack rows correctly', () => {
+    const bucket = createBucket(rows);
+    expect(bucket).toEqual(expectedBucket);
+  });
 
-test('random index in range', () => {
-  const iterator = Math.pow(expectedBucket.length, 3);
-  const randomIndexes = Array(iterator)
-    .fill()
-    .reduce(array => {
-      const index = randomIndex(expectedBucket);
-      !array.includes(index) && array.push(index);
-      return array;
-    }, [])
-    .sort();
+  it('random index in range', () => {
+    const iterator = Math.pow(expectedBucket.length, 3);
+    const randomIndexes = Array(iterator)
+      .fill()
+      .reduce(array => {
+        const index = randomIndex(expectedBucket);
+        !array.includes(index) && array.push(index);
+        return array;
+      }, [])
+      .sort();
 
-  expect(randomIndexes).toEqual(expectedIndexes);
+    expect(randomIndexes).toEqual(expectedIndexes);
+  });
 });
